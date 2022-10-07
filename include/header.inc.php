0fbf7f74df04e48af88abcd47def387ad17f3a5b
florentCourtin <92575172+florentCourtin@users.noreply.github.com>
2022-10-07 15:38:33 +0200

    CSS pour le header

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,10 +1,10 @@
 <header>
-	<h1>Développement web avancé</h1>
-    <nav>
-		<ul>
-            <li><img src="images/logo.png" alt="Continue mon &oelig;uvre" /></li>
-            <li>Se connecter</li>
-            <li>S'inscrire</li>
+  <nav>
+    <a href="index.php"><img src="images/logo.png" alt="Continue mon &oelig;uvre" id="logo-header"/></a>
+    <ul>
+      <li><a href="#">Se connecter</a></li>
+      <li><a href="#">S'inscrire</a></li>
 		</ul>
-    </nav>
+  </nav>
+  <h1>Développement web avancé</h1>
 </header>

f5b1b1de53501c5462f192bbf5981f98b7cdee81
Krystela21 <91313673+Krystela21@users.noreply.github.com>
2022-11-25 12:13:32 +0100

    Profil de qqn d'autre V.1
    La nouvelle fonction get_userInfos_else()
    Ajout de la page de profil_view.php
    (version 1.1)

---
+++ profil-view.php
@@ -0,0 +1,34 @@
+<!--PROFIL DE SON PROPRE PROFIL-->
+<?php 
+	$type = "article:author";
+    $titre = "profil-view";
+    if (isset($_GET["user"])&&!empty($_GET["user"])){
+        $profile = $_GET["user"];
+        $titre .= " - $profile";
+    }
+    
+	include "include/header.inc.php";
+?>
+	<main>
+		<div class="container mt-1">
+		<?php 
+			if(isset($_GET["user"]) && !empty($_GET["user"])){
+				if(isset($_SESSION['session']) && !empty($_SESSION['session']) && $_SESSION['session']==true){
+		    		require "./include/functions.inc.php";
+                    echo get_userInfos_else($_GET["user"]);
+				}
+				else{
+					echo "<p class='alert alert-warning'>Veuillez vous connecter pour accéder à cette page.</p>";
+				}
+			}
+			else{
+				echo "<p class='alert alert-warning'>Cette page n'existe pas ! <a href='https://continuemonoeuvre.alwaysdata.net/profil.php?user=christellaTest1'>Retour à la page d'accueil</a></p>";
+			}
+		?>
+		</div>
+	</main>
+
+	<?php include "include/footer.inc.php"; ?>
+	
+	</body>
+</html>

d3c206dbf2b0e29fe736629bbd8b07e94d0f20dd
alicemabille <alicemabille@yahoo.fr>
2022-12-09 19:57:56 +0100

    boutons de gif fonctionnels

--- scripts/script.js
+++ scripts/script.js
@@ -26,8 +26,14 @@ $("#featured_gif_button").click(function() {
     $.get(`https://tenor.googleapis.com/v2/featured?key=${TENOR_API_KEY}&client_key=continue+mon+oeuvre`, function(data, status){
         for(let i=0;i<data.results.length;i+=2){
             $("#gif-list").append(`<div class="preview_gif col-5 m-1">
-            <input type="image" src="${data.results[i].media_formats.tinygif.url}" class="gif rounded img-fluid alt="featured gif ${i}">
-            <input type="image" src="${data.results[i+1].media_formats.tinygif.url}" class="gif rounded img-fluid" alt="featured gif ${i}">
+            <form method="post">
+              <input type="hidden" name="gif" value="${data.results[i].media_formats.tinygif.url}" id="gif${i}" >
+              <input type="image" src="${data.results[i].media_formats.tinygif.url}" alt="featured gif ${i}" class="gif rounded img-fluid">
+            </form>
+            <form method="post">
+              <input type="hidden" name="gif" value="${data.results[i+1].media_formats.tinygif.url}" id="gif${i+1}" >
+              <input type="image" src="${data.results[i+1].media_formats.tinygif.url}" alt="featured gif ${i+1}" class="gif rounded img-fluid">
+            </form>
             </div>`);
         }
     });
@@ -39,8 +45,14 @@ $("#search_gif_button").click(function() {
     $.get(`https://tenor.googleapis.com/v2/search?key=${TENOR_API_KEY}&client_key=continue+mon+oeuvre&q=${q}`, function(data, status){
         for(let i=0;i<data.results.length;i+=2){
             $("#gif-list").append(`<div class="preview_gif col-5 m-1">
-            <input type="image" src="${data.results[i].media_formats.tinygif.url}" class="rounded img-fluid" alt="${q} gif ${i}">
-            <input type="image" src="${data.results[i+1].media_formats.tinygif.url}" class="rounded img-fluid alt="${q} gif ${i+1}">
+            <form method="post">
+              <input type="hidden" name="gif" value="${data.results[i].media_formats.tinygif.url}" id="gif${i}" >
+              <input type="image" src="${data.results[i].media_formats.tinygif.url}" alt="featured gif ${i}" class="gif rounded img-fluid">
+            </form>
+            <form method="post">
+              <input type="hidden" name="gif" value="${data.results[i+1].media_formats.tinygif.url}" id="gif${i+1}" >
+              <input type="image" src="${data.results[i+1].media_formats.tinygif.url}" alt="featured gif ${i+1}" class="gif rounded img-fluid">
+            </form>
             </div>`);
         }
     });
